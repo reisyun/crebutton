41664de1c04ad0b54ed5d40ca5989c5326dcf0ca
Rei <reisyun7@gmail.com>
2019-08-27 21:37:49 +0900

    Fix useDisabled

--- src/lib/hooks/useDisabled.jsx
+++ src/lib/hooks/useDisabled.jsx
@@ -3,18 +3,22 @@ import React from 'react';
 export default function useDisabled(duration) {
   const [disabled, setDisabled] = React.useState(false);
 
-  // 시간이 지나면 상태를 false 처리
-  const delay = (time, setState) => {
-    setTimeout(() => {
-      setState(false);
-    }, time);
-  };
-
   // 중복 클릭 방지
   const onDisabled = React.useCallback(() => {
-    disabled === false && setDisabled(true);
-    delay(duration, setDisabled);
+    // 이벤트가 오면 버튼 기능 정지 (disable = true)
+    if (disabled === false) {
+      setDisabled(true);
+    }
+
+    // 시간이 지나면 버튼 기능 활성 (disable = false)
+    _delay(duration, setDisabled);
   }, [disabled, duration]);
 
   return [disabled, onDisabled];
 }
+
+const _delay = (time, setState) => {
+  setTimeout(() => {
+    setState(false);
+  }, time);
+};
